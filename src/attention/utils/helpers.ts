1cae4b0124cc19e1316b3757cdc2376ac375dad9
felicia-haggqvist <144792260+felicia-haggqvist@users.noreply.github.com>
2024-04-16 13:40:49 +0200

    feat: update how attentionEl's position should be controlled (#15)
    * feat: add more placement options + WIP refactor useRecompute
    
    * feat: update floating-ui/dom package to latest version
    
    * feat: add more directions to AttentionState type + isDirectionVertical
    
    * feat: WIP return position from useRecompute()
    
    * feat: remove redundant return of position
    
    * feat: WIP fix arrow position adjustment for placements with -start or -end + add autoUpdate()
    
    * feat: add style for staticSides
    
    * WIP fix TypeError
    
    * feat: WIP adjust arrowEl fort -start -end positions
    
    * feat: update typo
    
    * feat: update comment
    
    * feat: rename cleanup to update
    
    * feat: WIP fix arrow position for -start and -end
    
    * feat: remove check for -end
    
    * feat: revert floatingOffset
    
    * feat: change to const
    
    * feat: refactor useRecompute
    
    * feat: WIP rename useRecompute to updatePosition + test to separate autopUpdate() from updatePosition()
    
    * feat: WIP fix autoUpdate
    
    * feat: WIP refactor useRecompute
    
    * feat: remove floating-ui calculations + add update as a parameter
    
    * feat: remove check if state.attentionEl exists
    
    * feat: remove redundant use of attentionEl in attentionState
    
    * feat: add await + remove () from waitForDOM
    
    * feat: add if-check for state.waitForDOM to fix failing vue test
    
    * feat: uninstall @floating-ui/dom
    
    * feat: revert uninstalling floating-ui/dom + add autoUpdatePosition function
    
    * feat: set state.arrowEl to arrowEl variable
    
    * feat: cleanup arrow-positioning
    
    * WIP debug computePosition
    
    * feat: return state in useRecompute()
    
    * feat: revert changes to computeCalloutArrow function
    
    * set fallbackAxisSideDirection to "start" in computePosition()
    
    * feat: position arrow correctly when placement has "start" or "end"
    
    * WIP add rotation transform to useRecompute + computeCalloutArrow
    
    * fix: return if referenceEl is undefined, fixing error in tests for vue
    
    * feat: add distance and skidding to AttentionState and set it as offset-options to offer more position-options for the floatingEl
    
    * WIP add IntersectionObserver to set state.isShowing to false when referenceEl is no longer in view
    
    * feat: comment out IntersectionObserver + add default value for state.distance + state.skidding
    
    * WIP testing out adding interserction observer
    
    * feat: default state.directionName to 'bottom'
    
    * feat: add default for directionName in computeCalloutArrow
    
    * feat: use flip + fallbackPlacements props to set flip()-floating-ui-middleware
    
    * feat: remove curly brackets
    
    * feat: remove commented out section regarding adding intersection observer
    
    * feat: remove fallbackAxisSideDirection + fallbackStrategy
    
    * feat: revert removing fallbackAxisSideDirection
    
    * feat: add more descriptive comments for the useRecompute function
    
    * feat: fix after comments
    
    * feat: fix typing issues + use opposites in staticSide
    
    * feat: add comment to explain fallbackAxisSideDirection
    
    * feat: update comments
    
    * feat: add safe guard for middleware
    
    * add @floating-ui/dom as a peerDependency
    
    * feat: remove unused arrowLabels + directions, remove export for rotation since it is only used internally
    
    * feat: add defaultDirectionName const + add return type for isDirectionVertical
    
    * use BOTTOM enum instead of defaultDirectionName
    
    * feat: revert removing directions
    
    * feat: use the enums instead of strings
    
    * feat: move in arrowDirectionClassname and add margin style
    
    * feat: clean up side + staticSide functions
    
    * feat: add borderTopLeftRadius + zIndex to arrow-style
    
    * feat: create applyArrowStyle function and move duplicated style in there
    
    * feat: Remake arrowDirection and arrowRotation into functions and use them instead to avoid duplication
    
    * feat: change calc according to figma sketches
    
    * feat: refactor oppposites
    
    * feat: use autoPlacement() when flip is false
    
    * fix: remove template literal
    
    * fix: remove floating-ui/dom as devDep

--- src/attention/utils/helpers.ts
+++ src/attention/utils/helpers.ts
@@ -1,98 +1,240 @@
-import { computePosition, flip, offset, shift, arrow, ReferenceElement } from "@floating-ui/dom";
+import {
+  computePosition,
+  flip,
+  offset,
+  shift,
+  arrow,
+  autoUpdate,
+  ReferenceElement,
+  autoPlacement,
+} from '@floating-ui/dom'
 
-const TOP = "top";
-const BOTTOM = "bottom";
-const LEFT = "left";
-const RIGHT = "right";
-type Directions = "top" | "right" | "bottom" | "left";
+export type Directions =
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+  
+const TOP_START: Directions = 'top-start'
+const TOP: Directions = 'top'
+const TOP_END: Directions = 'top-end'
+const RIGHT_START: Directions = 'right-start'
+const RIGHT: Directions = 'right'
+const RIGHT_END: Directions = 'right-end'
+const BOTTOM_START: Directions = 'bottom-start'
+const BOTTOM: Directions = 'bottom'
+const BOTTOM_END: Directions = 'bottom-end'
+const LEFT_START: Directions = 'left-start'
+const LEFT: Directions = 'left'
+const LEFT_END: Directions = 'left-end'
 
-export const opposites = {
+export const directions: Directions[] = [
+  TOP_START,
+  TOP,
+  TOP_END,
+  RIGHT_START,
+  RIGHT,
+  RIGHT_END,
+  BOTTOM_START,
+  BOTTOM,
+  BOTTOM_END,
+  LEFT_START,
+  LEFT,
+  LEFT_END]
+
+export const opposites: Record<Directions, Directions> = {
+  [TOP_START]: BOTTOM_START,
   [TOP]: BOTTOM,
+  [TOP_END]: BOTTOM_END,
+  [BOTTOM_START]: TOP_START,
   [BOTTOM]: TOP,
+  [BOTTOM_END]: TOP_END,
+  [LEFT_START]: RIGHT_START,
   [LEFT]: RIGHT,
+  [LEFT_END]: RIGHT_END,
+  [RIGHT_START]: LEFT_START,
   [RIGHT]: LEFT,
-};
-export const arrowLabels = {
-  [TOP]: "↑",
-  [BOTTOM]: "↓",
-  [LEFT]: "←",
-  [RIGHT]: "→",
-};
-export const directions = [TOP, BOTTOM, LEFT, RIGHT];
-export const rotation = {
+  [RIGHT_END]: LEFT_END,
+}
+
+const rotation: Record<Directions, number> = {
+  [LEFT_START]: -45,
   [LEFT]: -45,
+  [LEFT_END]: -45,
+  [TOP_START]: 45,
   [TOP]: 45,
+  [TOP_END]: 45,
+  [RIGHT_START]: 135,
   [RIGHT]: 135,
+  [RIGHT_END]: 135,
+  [BOTTOM_START]: -135,
   [BOTTOM]: -135,
-};
+  [BOTTOM_END]: -135,
+}
 
 export type AttentionState = {
-  isShowing?: boolean;
-  isCallout?: boolean;
-  actualDirection?: Directions;
-  directionName: Directions;
-  arrowEl?: HTMLElement | null;
-  attentionEl?: HTMLElement | null;
-  targetEl?: unknown;
-  top?: Boolean;
-  right?: Boolean;
-  bottom?: Boolean;
-  left?: Boolean;
-  tooltip?: Boolean;
-  popover?: Boolean;
-  callout?: Boolean;
-  noArrow?: Boolean;
-  waitForDOM?: () => void;
-};
+  isShowing?: boolean
+  isCallout?: boolean
+  actualDirection?: Directions
+  directionName?: Directions
+  arrowEl?: HTMLElement | null
+  attentionEl?: HTMLElement | null
+  flip?: Boolean
+  fallbackPlacements?: Directions[]
+  targetEl?: ReferenceElement | null
+  noArrow?: Boolean
+  distance?: number
+  skidding?: number
+  waitForDOM?: () => void
+}
 
-const middlePosition = "calc(50% - 7px)";
-const isDirectionVertical = (name: string) => [TOP, BOTTOM].includes(name);
-function computeCalloutArrow({
-  actualDirection,
-  directionName,
-  arrowEl,
-}: AttentionState) {
-  if (!arrowEl) return;
+const middlePosition: string = 'calc(50% - 7px)'
+const isDirectionVertical = (name: Directions): boolean =>
+  ([TOP_START, TOP, TOP_END, BOTTOM_START, BOTTOM, BOTTOM_END] as Directions[]).includes(name)
 
-  actualDirection = directionName;
-  const directionIsVertical = isDirectionVertical(directionName);
-  arrowEl.style.left = directionIsVertical ? middlePosition : "";
-  arrowEl.style.top = !directionIsVertical ? middlePosition : "";
+export const arrowDirectionClassname = (dir: Directions) => {
+    let direction: Directions
+    if (/-/.test(dir)) {
+      direction = dir
+        .split('-')
+        .map((d) => d.charAt(0).toUpperCase() + d.slice(1))
+        .join('') as Directions
+    } else {
+      direction = dir.charAt(0).toUpperCase() + dir.slice(1) as Directions
+    }
+    return direction
+  }
+
+  const side = (dir: Directions): Directions => dir.split('-')[0] as Directions
+  const staticSide = (dir: Directions): Directions => opposites[side(dir)]
+  const arrowDirection = (dir: Directions): Directions => opposites[dir]
+  const arrowRotation = (dir: Directions): number => rotation[arrowDirection(dir)]
+
+  const applyArrowStyles = (arrowEl: HTMLElement, arrowRotation: number, dir: Directions) => {
+    Object.assign(arrowEl?.style, {
+      borderTopLeftRadius: '4px',
+      zIndex: 1,
+    // border alignment is off by a fraction of a pixel, this fixes it
+      [`margin${arrowDirectionClassname(staticSide(dir))}`]: '-0.5px',
+      transform: `rotate(${arrowRotation}deg)`,
+    });
+  }
+
+  function computeCalloutArrow({
+    actualDirection,
+    directionName = BOTTOM,
+    arrowEl,
+  }: AttentionState) {
+    if (!arrowEl) return
+    
+    actualDirection = directionName
+
+  const directionIsVertical: boolean = isDirectionVertical(directionName)
+
+  Object.assign(arrowEl?.style || {}, {
+    left: directionIsVertical ? middlePosition : '',
+    top: !directionIsVertical ? middlePosition : '',
+  })
+  applyArrowStyles(arrowEl, arrowRotation(actualDirection), actualDirection)
 }
 
 export async function useRecompute(state: AttentionState) {
-  if (!state.isShowing) return; // we're not currently showing the element, no reason to recompute
-  await state?.waitForDOM?.(); // wait for DOM to settle before computing
-  if (state.isCallout) return computeCalloutArrow(state); // we don't move the callout box, only its arrow
-  const position = await computePosition(
-    state.targetEl as ReferenceElement,
-    state.attentionEl as HTMLElement,
-    {
-      placement: state.directionName,
-      middleware: [
-        // Should we make this configurable, but have these as sane defaults?
-        flip(),
-        offset(8),
-        shift({ padding: 16 }),
-        // @ts-ignore
-        arrow({ element: state.noArrow ? undefined : state.arrowEl }), // FIXME
-      ],
-    }
-  );
-  // @ts-ignore
-  state.actualDirection = position.placement;
-  Object.assign(state.attentionEl?.style || {}, {
-    left: "0",
-    top: "0",
-    transform: `translate3d(${Math.round(position.x)}px, ${Math.round(
-      position.y
-    )}px, 0)`,
-  });
-  // @ts-ignore
-  let { x, y } = position.middlewareData.arrow;
-
-  if (state.arrowEl) {
-    state.arrowEl.style.left = x ? x + "px" : "";
-    state.arrowEl.style.top = y ? y + "px" : "";
+  if (!state?.isShowing) return // we're not currently showing the element, no reason to recompute
+  if (state?.waitForDOM) {
+    await state?.waitForDOM() // wait for DOM to settle before computing
   }
+  if (state?.isCallout) return computeCalloutArrow(state)
+  
+  if (!state?.targetEl || !state?.attentionEl) return
+  
+  const targetEl: ReferenceElement = state?.targetEl
+  const attentionEl: HTMLElement = state?.attentionEl
+  
+  computePosition(targetEl, attentionEl, {
+    placement: state?.directionName ?? BOTTOM,
+    middleware: [
+      offset({ mainAxis: state?.distance ?? 8, crossAxis: state?.skidding ?? 0}), // offers flexibility over how to place the attentionEl towards its targetEl both on the x and y axis (horizontally and vertically).
+      state?.flip && flip({ //when flip is set to true it will move the attentionEl's placement to its opposite side or to the preferred placements if fallbackPlacements has a value
+        fallbackAxisSideDirection: 'start', // the preferred placement axis fit when flip is set to true and fallbackPlacements does not have a value. 'start' represents 'top' or 'left'.
+        fallbackPlacements: state?.fallbackPlacements,
+      }),
+      !state?.flip && autoPlacement(), // when flip is set to false, it will instead call autoPlacement() that will choose the placement that has the most space available automatically. You can only use either flip() or autoPlacement(), and not combined. 
+      shift({ padding: 16}),
+      !state?.noArrow && state?.arrowEl && arrow({ element: state?.arrowEl }),
+    ],
+  }).then(({ x, y, middlewareData, placement }) => {
+    state.actualDirection = placement
+    
+    Object.assign(attentionEl?.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    })
+    const isRtl = window.getComputedStyle(attentionEl).direction === 'rtl' //checks whether the text direction of the attentionEl is right-to-left. Helps to calculate the position of the arrowEl and ensure proper alignment 
+    const arrowPlacement: string = arrowDirection(placement).split('-')[1]
+    
+    if (middlewareData?.arrow && state?.arrowEl) {
+      const arrowEl: HTMLElement = state?.arrowEl
+      const { x, y } = middlewareData?.arrow
+      let top = ''
+      let right = ''
+      let bottom = ''
+      let left = ''
+
+      // calculates the arrow-position depending on if placement has 'start' or 'end':
+      if (arrowPlacement === 'start') {
+        const value =
+          typeof x === 'number'
+            ? `calc(33px - ${arrowEl?.offsetWidth / 2}px)`
+            : ''
+        top =
+          typeof y === 'number'
+            ? `calc(33px -  ${arrowEl?.offsetWidth / 2}px)`
+            : ''
+        right = isRtl ? value : ''
+        left = isRtl ? '' : value
+      } else if (arrowPlacement === 'end') {
+        const value =
+          typeof x === 'number'
+            ? `calc(33px - ${arrowEl?.offsetWidth / 2}px)`
+            : ''
+        right = isRtl ? '' : value
+        left = isRtl ? value : ''
+        bottom =
+          typeof y === 'number'
+            ? `calc(33px - ${arrowEl?.offsetWidth / 2}px)`
+            : ''
+      } else {
+        left = typeof x === 'number' ? `${x}px` : ''
+        top = typeof y === 'number' ? `${y}px` : ''
+      }
+
+      Object.assign(arrowEl?.style || {}, {
+        top,
+        right,
+        bottom,
+        left,
+      })
+      applyArrowStyles(arrowEl, arrowRotation(placement), placement)
+    }
+  })
+
+  return state
+}
+
+export const autoUpdatePosition = (state: AttentionState) => {
+  // computePosition is only run once, so we need to wrap autoUpdate() around useRecompute() in order to recompute the attentionEl's position
+  // autoUpdate adds event listeners that are triggered on resize and on scroll and will keep calling the useRecompute(). 
+  // autoUpdate returns a cleanup() function that removes the event listeners.
+  if (!state?.targetEl || !state?.attentionEl) return 
+  return autoUpdate(state?.targetEl, state?.attentionEl, () => {
+    useRecompute(state)
+  })
 }
